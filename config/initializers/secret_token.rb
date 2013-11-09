7cc8e7bee8bcca52c7d5949a173a8648e614991a
Astr0surf3r <astr0surf3r@gmail.com>
2013-11-08 19:27:01 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoipx::Application.config.secret_token = 'ed53f322c96968739503e50501a0e9710a5acf4ff2a4b1ac461bf3239cbf7c7e98184faa7ab883710cc2b379e73ba167f8f6f371acdf9fb007a30877726d7914'
